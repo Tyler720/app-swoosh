fbd96c65fd268566f75665a8ed8ef42336786e87
Tyler Cantlon <Tyler@Tyler-Cantlons-MacBook.local>
2019-01-07 18:22:29 -0600

    pass information through segues

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Tyler Cantlon on 1/6/19.
+//  Copyright © 2019 Tyler. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
